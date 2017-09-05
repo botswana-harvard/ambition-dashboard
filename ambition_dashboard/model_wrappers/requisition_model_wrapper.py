28d17ab7b2701e2ac85378dd296b19a3abd0ba3a
Leslie <leslie.kep@gmail.com>
2017-09-05 09:54:17 +0200

    add missing panel name property

--- ambition_dashboard/model_wrappers/requisition_model_wrapper.py
+++ ambition_dashboard/model_wrappers/requisition_model_wrapper.py
@@ -4,6 +4,7 @@ from .crf_model_wrapper import CrfModelWrapper
 class RequisitionModelWrapper(CrfModelWrapper):
 
     model = 'ambition_subject.subjectrequisition'
+    requisition_panel_name = None
     next_url_attrs = ['appointment', 'subject_identifier']
     querystring_attrs = ['subject_visit', 'panel_name']
 
@@ -17,3 +18,7 @@ class RequisitionModelWrapper(CrfModelWrapper):
     @property
     def subject_identifier(self):
         return self.object.subject_visit.subject_identifier
+
+    @property
+    def panel_name(self):
+        return self.requisition_panel_name
