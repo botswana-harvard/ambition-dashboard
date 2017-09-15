8234783150809bf3e0bd8ad3479e620f76f9a4f9
Leslie <leslie.kep@gmail.com>
2017-09-15 15:46:08 +0200

    add regimen to querystring

--- ambition_dashboard/model_wrappers/crf_model_wrapper.py
+++ ambition_dashboard/model_wrappers/crf_model_wrapper.py
@@ -1,5 +1,6 @@
 from django.apps import apps as django_apps
 from edc_model_wrapper import ModelWrapper
+from django.core.exceptions import ObjectDoesNotExist
 
 
 class CrfModelWrapper(ModelWrapper):
@@ -7,7 +8,8 @@ class CrfModelWrapper(ModelWrapper):
     next_url_name = django_apps.get_app_config(
         'ambition_dashboard').dashboard_url_name
     next_url_attrs = ['appointment', 'subject_identifier']
-    querystring_attrs = ['subject_visit']
+    querystring_attrs = ['subject_visit', 'regimen']
+    subject_randomization_model = 'ambition_rando.subjectrandomization'
 
     @property
     def subject_visit(self):
@@ -20,3 +22,15 @@ class CrfModelWrapper(ModelWrapper):
     @property
     def subject_identifier(self):
         return self.object.subject_visit.subject_identifier
+
+    @property
+    def regimen(self):
+        model_cls = django_apps.get_model(self.subject_randomization_model)
+        try:
+            obj = model_cls.objects.get(
+                subject_identifier=self.object.subject_visit.appointment.subject_identifier)
+        except ObjectDoesNotExist:
+            rx_arm = None
+        else:
+            rx_arm = ' '.join(obj.rx.split('_')).title()
+        return rx_arm
