e435a31be6de92c2b72240aaaf6aae838eeec787
Itumeleng Mosweu <imosweuk@gmail.com>
2017-12-08 17:00:52 +0200

    add offstudy required in dashboard view

--- ambition_dashboard/tests/models.py
+++ ambition_dashboard/tests/models.py
@@ -5,12 +5,62 @@ from edc_base.model_mixins import BaseUuidModel
 
 class Appointment(BaseUuidModel):
 
+    visit_code = models.CharField(
+        max_length=25,
+        null=True,
+        editable=False)
+
+    appt_datetime = models.DateTimeField(
+        verbose_name=('Appointment date and time'),
+        db_index=True)
+
     subject_identifier = models.CharField(max_length=25)
 
 
 class SubjectVisit(BaseUuidModel):
 
     appointment = models.OneToOneField(Appointment, on_delete=PROTECT)
+
+    subject_identifier = models.CharField(max_length=25)
+
+    visit_code = models.CharField(
+        max_length=25,
+        null=True,
+        editable=False)
+
+
+class BloodResultModel(models.Model):
+
+    subject_visit = models.ForeignKey(SubjectVisit, on_delete=PROTECT)
+
+    platelets = models.IntegerField(
+        null=True,)
+
+    absolute_neutrophil = models.DecimalField(
+        decimal_places=2,
+        max_digits=4,
+        null=True,)
+
+    alt = models.IntegerField(
+        null=True)
+
+
+class DeathReport(models.Model):
+
+    subject_identifier = models.CharField(max_length=25)
+
+
+class StudyTerminationConclusion(models.Model):
+
+    subject_identifier = models.CharField(max_length=25)
+
+
+class SubjectLocator(BaseUuidModel):
+
+    subject_identifier = models.CharField(max_length=25)
+
+
+class SubjectOffstudy(BaseUuidModel):
 
     subject_identifier = models.CharField(max_length=25)
 
@@ -41,8 +91,3 @@ class SubjectConsent(BaseUuidModel):
     initials = models.CharField(max_length=25, default='XX')
 
     first_name = models.CharField(max_length=25, default='NOAM')
-
-
-class SubjectLocator(BaseUuidModel):
-
-    subject_identifier = models.CharField(max_length=25)
